f7b750555262c74cadf0947df3a7bd1a87f6ab23
SajeevRaj5 <sajeevraj.1989@gmail.com>
2020-03-07 11:59:04 +0400

    Initial commit
    Added all the required files and designed the recording screen

---
+++ iTranslateSample01/Record/ViewController/RecordViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RecordViewController.swift
+//  iTranslateSample01
+//
+//  Created by Sajeev on 3/7/20.
+//  Copyright © 2020 iTranslate. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func showRecordingButtonAction(_ sender: UIButton) {
+    }
+}
