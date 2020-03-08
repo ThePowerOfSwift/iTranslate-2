73ee17043bbdcef7ae4010aa064fc771e6d1b7e9
SajeevRaj <sajeevraj.1989@gmail.com>
2020-03-08 15:12:51 +0400

    Created Model class for Record
    Implemented Model class for saving the data locally

---
+++ iTranslateSample01/Record/Model/Record.swift
@@ -0,0 +1,15 @@
+//
+//  Record.swift
+//  iTranslateSample01
+//
+//  Created by Sajeev on 3/8/20.
+//  Copyright © 2020 iTranslate. All rights reserved.
+//
+
+import Foundation
+
+class Record {
+    var id = ""
+    var filePath = ""
+    
+}
