6a66bc540ad71709ba1578a6d6c7a9f8fc9e1c39
SajeevRaj <sajeevraj.1989@gmail.com>
2020-03-11 20:58:11 +0400

    Removed unused methods

--- iTranslate/RecordList/ViewController/RecordListViewCell.swift
+++ iTranslate/RecordList/ViewController/RecordListViewCell.swift
@@ -12,21 +12,9 @@ class RecordListViewCell: UITableViewCell {
 
     @IBOutlet weak var durationLabel: UILabel?
     @IBOutlet weak var nameLabel: UILabel?
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    
+
     func configureView(record: RecordDisplayViewModel) {
         nameLabel?.text = record.name
         durationLabel?.text = record.duration
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
